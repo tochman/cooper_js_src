31b4a59a10e417207e68afd2e745bd996886afb5
tochman <thomas@nocebo.se>
2016-03-30 13:03:27 +0200

    adds 'getCooperResultsForMales' with specs

--- spec/cooper_spec.js
+++ spec/cooper_spec.js
@@ -1,7 +1,10 @@
 describe("Person", function() {
 
   beforeEach(function() {
-    person = new Person({gender: 'Male', age: 46});
+    person = new Person({
+      gender: 'Male',
+      age: 46
+    });
   });
 
   it("should have gender", function() {
@@ -12,6 +15,89 @@ describe("Person", function() {
   it("should have age", function() {
     expect(person.age).toBeDefined();
     expect(person.age).toBe(46);
+  });
+
+  describe("Calculate results", function() {
+    beforeEach(function() {
+      spyOn(person, 'getCooperResultsForMales').and.callThrough();
+      person.assessCooper(2400);
+    });
+
+    it("should return a message", function() {
+      expect(person.cooperMessage).toBeDefined();
+      expect(person.getCooperResultsForMales).toHaveBeenCalled();
+      expect(person.cooperMessage).toBe('Above average');
+    });
+
+    describe('Male, 20', function() {
+      beforeEach(function() {
+        person = new Person({
+          gender: 'Male',
+          age: 20
+        });
+      });
+
+      it('1400 m should equal "Poor"', function(){
+        person.assessCooper(1400);
+        expect(person.cooperMessage).toBe('Poor');
+      })
+
+      it('1800 m should equal "Below average"', function(){
+        person.assessCooper(1800);
+        expect(person.cooperMessage).toBe('Below average');
+      })
+
+      it('2300 m should equal "Average"', function(){
+        person.assessCooper(2300);
+        expect(person.cooperMessage).toBe('Average');
+      })
+
+      it('2500 m should equal "Above average"', function(){
+        person.assessCooper(2500);
+        expect(person.cooperMessage).toBe('Above average');
+      })
+
+      it('2800 m should equal "Excellent"', function(){
+        person.assessCooper(2800);
+        expect(person.cooperMessage).toBe('Excellent');
+      })
+
+    });
+
+    describe('Male, 51', function() {
+      beforeEach(function() {
+        person = new Person({
+          gender: 'Male',
+          age: 51
+        });
+      });
+
+      it('1290 m should equal "Poor"', function(){
+        person.assessCooper(1290);
+        expect(person.cooperMessage).toBe('Poor');
+      })
+
+      it('1599 m should equal "Below average"', function(){
+        person.assessCooper(1599);
+        expect(person.cooperMessage).toBe('Below average');
+      })
+
+      it('1999 m should equal "Average"', function(){
+        person.assessCooper(1999);
+        expect(person.cooperMessage).toBe('Average');
+      })
+
+      it('2400 m should equal "Above average"', function(){
+        person.assessCooper(2400);
+        expect(person.cooperMessage).toBe('Above average');
+      })
+
+      it('2401 m should equal "Excellent"', function(){
+        person.assessCooper(2401);
+        expect(person.cooperMessage).toBe('Excellent');
+      })
+
+    });
   });
 
 });
--- src/cooper.js
+++ src/cooper.js
@@ -2,3 +2,102 @@ function Person(attr) {
   this.gender = attr.gender;
   this.age = attr.age;
 };
+
+
+Person.prototype.assessCooper = function(distance) {
+
+  if (this.gender.toLowerCase() == 'male') {
+    this.cooperMessage = this.getCooperResultsForMales(distance);
+  } else {
+    this.cooperMessage = this.getCooperResultsForFemales(distance);
+  }
+};
+
+Person.prototype.getCooperResultsForMales = function(distance) {
+  var age = this.age;
+  switch (true) {
+    case (age >= 20 && age < 29):
+      switch (true) {
+        case (distance < 1600):
+          return 'Poor';
+          break;
+        case (distance >= 1600 && distance < 2199):
+          return 'Below average';
+          break;
+        case (distance >= 2200 && distance < 2399):
+          return 'Average';
+          break;
+        case (distance >= 2400 && distance < 2800):
+          return 'Above average';
+          break;
+        default:
+          return 'Excellent'
+      }
+      break;
+
+    case (age >= 30 && age < 39):
+      switch (true) {
+        case (distance < 1500):
+          return 'Poor';
+          break;
+        case (distance >= 1500 && distance < 1999):
+          return 'Below average';
+          break;
+        case (distance >= 1999 && distance < 2299):
+          return 'Average';
+          break;
+        case (distance >= 2300 && distance < 2700):
+          return 'Above average';
+          break;
+        case (distance > 2700):
+          return 'Excellent';
+          break;
+      }
+      break;
+
+    case (age >= 40 && age < 49):
+    switch (true) {
+      case (distance < 1400):
+        return 'Poor';
+        break;
+      case (distance >= 1400 && distance < 1699):
+        return 'Below average';
+        break;
+      case (distance >= 1700 && distance < 2099):
+        return 'Average';
+        break;
+      case (distance >= 2100 && distance < 2500):
+        return 'Above average';
+        break;
+      case (distance >= 2500):
+        return 'Excellent';
+        break;
+    }
+      break;
+
+    case (age >= 50):
+    switch (true) {
+      case (distance < 1300):
+        return 'Poor';
+        break;
+      case (distance >= 1300 && distance <= 1599):
+        return 'Below average';
+        break;
+      case (distance >= 1600 && distance <= 1999):
+        return 'Average';
+        break;
+      case (distance >= 2000 && distance <= 2400):
+        return 'Above average';
+        break;
+      case (distance >= 2400):
+        return 'Excellent';
+        break;
+    }
+      break;
+
+  }
+}
+
+Person.prototype.getCooperResultsForFemales = function(distance) {
+  return 'Excelent';
+}
